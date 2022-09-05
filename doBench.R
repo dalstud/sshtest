127f018a27fc3bf26e6628e14e57ee067a78c6b4
thor <thor@cphbusiness.dk>
2022-09-05 16:29:57 +0200

    exercise

--- doBench.R
+++ doBench.R
@@ -1,36 +1,31 @@
 library(readr) # CSV file I/O, e.g. the read_csv function
-library(readr)
-library(Rtsne)
 library(doParallel)
 library(benchmarkme)
 
 # prepare df
 bnames=c("user","ram","cpumodel","cpuhastighed","cores","crossprodI","lmI","sortI")
-mydf <- data.frame(matrix(ncol = 8, nrow = 0)) 
-names(mydf) <- bnames
+# create dataframe for these columnames
+#mydf = <create df>
 
-# now gather information
+# now gather information from benchmarkme
 res <- benchmark_matrix_cal()
-rescpu=get_cpu()
-vuser = "kurt"
-vram = get_ram()
-v1=res[7,1]
-v2=res[10,1]
-v3=res[13,1]
+vuser = "<din bruger>"
+vram = 
+#v1=<first crossprod>
+#v2=<first lm>
+#v3=<first sort>
 
 #prepare modelfields
-modarr = unlist(strsplit(rescpu$model_name,"@"))
-vmod=modarr[[1]]
-vspeed=modarr[[2]]
-vcores=rescpu$no_of_cores
+  
+# now extract info
+  
+#vmod=<model>
+#vspeed=<processor ghz>
+#vcores=<num of cores>
+
 # collect into vector
 
 resv = c(vuser,vram,vmod,vspeed,vcores,v1,v2,v3)
-names(resv) <- bnames
 mydf <- rbind(mydf,resv)
-mydf <- mydf[-c(2),]
 
-# write to file
-write_csv(mydf,"newout.csv")
-
-
+# write to file "newout.csv"
